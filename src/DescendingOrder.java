35c0afc07b4ad59da07d43345e293d7c00ea4bcf
Igor Ryabchuk <igor.ryabchuk@tesco.com>
2018-09-05 16:16:37 +0100

    Sort the number in descending order of digits

--- src/DescendingOrder.java
+++ src/DescendingOrder.java
@@ -1,15 +1,12 @@
 //http://www.codewars.com/kata/descending-order/train/java
 
-import java.util.Arrays; //just for printing it out
-
 public class DescendingOrder {
   public static int sortDesc(final int num) {
     String numToString = Integer.toString(num);
     String [] stringArray = numToString.split("");
-    String [] sortedArray = bubbleSort(stringArray);
-    System.out.print(Arrays.toString (stringArray));
-    System.out.print(Arrays.toString (sortedArray));
-    return num;
+    bubbleSort(stringArray);
+    String newNumber = String.join ("",stringArray);
+    return Integer.parseInt (newNumber);
   }
 
   private static void bubbleSort(String [] array) {
@@ -18,7 +15,7 @@ public class DescendingOrder {
     for (int m = arrayLength; m >= 0; m--) {
       for (int i = 0; i < arrayLength - 1; i++) {
         counter = i + 1;
-        if (array[i] < array[counter]) {
+        if (array[i].compareTo (array[counter]) < 0) {
           swapCharacters(i, counter, array);
         }
       }
@@ -27,8 +24,8 @@ public class DescendingOrder {
 
   private static void swapCharacters (int i, int j, String[] array) {
     String temp;
-
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
-
-// Check https://docs.oracle.com/javase/7/docs/api/java/lang/String.html#compareTo(java.lang.String)
